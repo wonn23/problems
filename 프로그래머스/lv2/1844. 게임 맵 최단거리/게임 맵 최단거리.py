1f95c2d0d50e37a515ff58e526ac22d48ab7e990
Changwon Heo <wonn23@naver.com>
2023-09-26 01:57:33 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 5.76 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,36 +1,33 @@
 from collections import deque
 
-
+def adjacent_node(x,y,N,M):
+    adjacent = []
+    for dx, dy in [(-1,0),(1,0),(0,-1),(0,1)]: # 상하좌우
+        nx, ny = x + dx, y + dy
+        if 0<= nx < N and 0<= ny < M:
+            adjacent.append((nx,ny))
+    return adjacent
+    
 def solution(maps):
-    n = len(maps)  # 행
-    m = len(maps[0])  # 열
+    N = len(maps)
+    M = len(maps[0])
+    
+    visited = [[False for _ in range(M)] for _ in range(N)]
 
-    # 상하좌우 이동 좌표
-    dx = [-1, 1, 0, 0]
-    dy = [0, 0, -1, 1]
-
-    visited = [[False for i in range(m)] for j in range(n)]  # 2D로 방문 행렬 초기화
-
+    x,y,answer = 0,0,1
     q = deque()
-    x, y, distance = 0, 0, 1  # x,y 위치와 제자리인 것 세줘야해서 1이다
-    q.append([x, y, distance])
-    visited[x][y] = True
-
+    q.append((x, y, answer))
+    visited[0][0] = True
+    
     while q:
-        x, y, distance = q.popleft()
-        if x == n - 1 and y == m - 1:  # x,y가 도착지점에 도달했는지 먼저 확인
-            return distance
-
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if (
-                0 <= nx < n
-                and 0 <= ny < m
-                and maps[nx][ny] == 1
-                and visited[nx][ny] == False
-            ):
-                visited[nx][ny] = True
-                q.append([nx, ny, distance+1])
-
-    return -1  # while을 모두 빠져나왔다는 것은 도착지점에 도달 못했다는 것을 의미한다
+        x, y, answer = q.popleft()
+        
+        if x == N-1 and y == M-1:
+            return answer
+        adjacent = adjacent_node(x,y,N,M)
+        for i, j in adjacent:
+            if not visited[i][j] and maps[i][j] == 1:
+                visited[i][j] = True
+                q.append((i,j,answer+1))
+    
+    return -1
