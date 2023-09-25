2f9c4590459e2b920aab0ee942c29bca3d874c35
[AI7/2반] 허창원 <wonn22@gmail.com>
2023-09-26 02:02:24 +0900

    저장

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -1,38 +1,41 @@
 import sys
 from collections import deque
 
+
 def dfs(graph, v, visited):
-    visited[v] = 1        
-    print(v, end = " ")
-    
+    visited[v] = 1
+    print(v, end=" ")
+
     for i in range(1, node + 1):
         if not visited[i] and graph[v][i] == 1:
-              dfs(graph, i, visited)
+            dfs(graph, i, visited)
+
 
 def bfs(graph, v, visited):
     q = deque()
     q.append(v)
     visited[v] = True
-    
+
     while q:
         v = q.popleft()
-        print(v, end=' ')
+        print(v, end=" ")
         for i in range(1, node + 1):
             if not visited[i] and graph[v][i] == 1:
                 q.append(i)
-                visited[i] = True                
-                
-node, edge, start = map(int, sys.stdin.readline().split())
+                visited[i] = True
 
-graph = [[0] * (node+1) for _ in range(node+1)]
+
+node, edge, start = map(int, input().split())
+
+graph = [[0] * (node + 1) for _ in range(node + 1)]
 
 for i in range(edge):
-    a, b = map(int, sys.stdin.readline().split())
+    a, b = map(int, input().split())
     graph[a][b] = graph[b][a] = 1
-    
-visited = [False] * (node+1)
+
+visited = [False] * (node + 1)
 dfs(graph, start, visited)
 print()
 
-visited = [False] * (node+1)
+visited = [False] * (node + 1)
 bfs(graph, start, visited)
