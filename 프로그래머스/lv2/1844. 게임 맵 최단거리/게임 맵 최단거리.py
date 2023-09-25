fd3b3ec06bde116c439a5e37c9d4041dffdd5a25
[AI7/2반] 허창원 <wonn22@gmail.com>
2023-09-26 02:05:25 +0900

    게임 맵 최단거리에 설명 주석 추가

--- 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,33 +1,38 @@
 from collections import deque
 
-def adjacent_node(x,y,N,M):
+
+def adjacent_node(x, y, N, M):
     adjacent = []
-    for dx, dy in [(-1,0),(1,0),(0,-1),(0,1)]: # 상하좌우
+    for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:  # 상하좌우
         nx, ny = x + dx, y + dy
-        if 0<= nx < N and 0<= ny < M:
-            adjacent.append((nx,ny))
+        if 0 <= nx < N and 0 <= ny < M:
+            adjacent.append((nx, ny))
     return adjacent
-    
+
+
 def solution(maps):
     N = len(maps)
     M = len(maps[0])
-    
-    visited = [[False for _ in range(M)] for _ in range(N)]
 
-    x,y,answer = 0,0,1
+    visited = [[False for _ in range(M)] for _ in range(N)]  # visited 초기화 방법 기억하기
+
+    x, y, answer = 0, 0, 1
     q = deque()
     q.append((x, y, answer))
     visited[0][0] = True
-    
+
     while q:
         x, y, answer = q.popleft()
-        
-        if x == N-1 and y == M-1:
+
+        if x == N - 1 and y == M - 1:
             return answer
-        adjacent = adjacent_node(x,y,N,M)
+
+        # adjacent_node(x, y)은 N,M이 정의 되어있지 않아 문제발생함
+        adjacent = adjacent_node(x, y, N, M)
+
         for i, j in adjacent:
             if not visited[i][j] and maps[i][j] == 1:
                 visited[i][j] = True
-                q.append((i,j,answer+1))
-    
+                q.append((i, j, answer + 1))
+
     return -1
