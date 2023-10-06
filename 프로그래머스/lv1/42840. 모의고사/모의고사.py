e2de63fc8fe5d4f134321a7a819510257d7557c7
ChangWon Heo <wonn23@naver.com>
2023-10-06 14:42:38 +0900

    [level 1] Title: 모의고사, Time: 1.71 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv1/42840. 모의고사/모의고사.py
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.py
@@ -1,25 +1,28 @@
 def solution(answers):
     answer = []
-    num1 = [1,2,3,4,5] * 2000
-    num2 = [2,1,2,3,2,4,2,5] * 1250
-    num3 = [3,3,1,1,2,2,4,4,5,5] * 1000
+    give_up1 = [1,2,3,4,5] * 2000
+    give_up2 = [2,1,2,3,2,4,2,5] * 1250
+    give_up3 = [3,3,1,1,2,2,4,4,5,5] * 1000
     
-    num1_answer = 0
-    num2_answer = 0
-    num3_answer = 0
-    for i in range(len(answers)):
-        if num1[i] == answers[i]:
-            num1_answer +=1
-        if num2[i] == answers[i]:
-            num2_answer +=1
-        if num3[i] == answers[i]:
-            num3_answer +=1
-    max_num = max(num1_answer,num2_answer,num3_answer)
-    if max_num == num1_answer:
-        answer.append(1)
-    if max_num == num2_answer:
-        answer.append(2)
-    if max_num == num3_answer:
-        answer.append(3)    
-               
-    return answer
+    counts = []
+    count1 = count2 = count3 = 0
+    
+    for index, num in enumerate(answers):
+        if num == give_up1[index]:
+            count1 += 1
+        if num == give_up2[index]:
+            count2 += 1
+        if num == give_up3[index]:
+            count3 += 1
+    
+    counts.append((1,count1))
+    counts.append((2,count2))
+    counts.append((3,count3))
+    
+    counts.sort(key = lambda x: x[1],reverse = True)
+    if counts[2][1] == counts[0][1]:
+        return sorted([counts[0][0],counts[1][0],counts[2][0]])
+    elif counts[1][1] == counts[0][1]:
+        return sorted([counts[0][0],counts[1][0]])
+        
+    return [counts[0][0]]
