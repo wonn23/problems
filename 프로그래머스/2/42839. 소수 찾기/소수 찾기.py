cce79f859d1afe98abe4514308f316798070f99d
ChangWon Heo <wonn23@naver.com>
2023-10-12 00:55:07 +0900

    [level 2] Title: 소수 찾기, Time: 0.30 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -1,26 +1,29 @@
-from itertools import permutations
 import math
+from itertools import permutations
+
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, int(math.sqrt(n)) + 1):
+        if n % i == 0:
+            return False
+    return True
+
 def solution(numbers):
-    numbers_list = list(numbers)
-    result = []
-    
-    for i in range(len(numbers_list)+1):
-        result = result + list(permutations((numbers_list),i))
-    result = set(result)
-    
-    new_list = []
-    for i in result:
-        is_decial = ""
-        for j in i:
-            is_decial += j
-        if i and int(is_decial) >= 2:
-            new_list.append(int(is_decial))
-    
     answer = 0
-    for i in set(new_list):
-        for j in range(2,int(math.sqrt(i))+1):
-            if i % j == 0:
-                break
-        else:    
-            answer += 1
+    num_list = list(numbers)
+    
+    prime_set = set()
+    
+    for i in range(1, len(num_list) + 1):
+        perm_comb = permutations(num_list, i)
+        
+        for perm in perm_comb:
+            num_str = ''.join(perm)
+            num_int = int(num_str)
+            
+            if is_prime(num_int) and num_int not in prime_set:
+                prime_set.add(num_int)
+                answer += 1
+    
     return answer
