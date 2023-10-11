e72e8b5f7fae6283bab2908e99d2b9ee0b20af7c
ChangWon Heo <wonn23@naver.com>
2023-10-12 00:18:31 +0900

    [level 2] Title: 소수 찾기, Time: 0.39 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -1,34 +1,26 @@
+from itertools import permutations
 import math
-import itertools
-def is_prime_num(n):
-    if n <= 1:
-        return False
-    if n == 2:
-        return True
-    if n % 2 == 0:
-        return False
-    
-    sqrt_n = int(math.sqrt(n))
-    for i in range(3, sqrt_n + 1, 2):
-        if n % i == 0:
-            return False
-    
-    return True
-
 def solution(numbers):
+    numbers_list = list(numbers)
+    result = []
+    
+    for i in range(len(numbers_list)+1):
+        result = result + list(permutations((numbers_list),i))
+    result = set(result)
+    
+    new_list = []
+    for i in result:
+        is_decial = ""
+        for j in i:
+            is_decial += j
+        if i and int(is_decial) >= 2:
+            new_list.append(int(is_decial))
+    
     answer = 0
-    num = list(numbers)
-    prime_set = set()
-    for i in range(1, len(num) + 1):
-        result = ''
-        a = list(itertools.permutations(num, i))
-        for j in a:
-            # 튜플 안의 문자열을 합치기
-            combined = ''.join(j)
-            # 합친 문자열을 숫자로 변환
-            number = int(combined)
-            if is_prime_num(number):
-                prime_set.add(number)
-        print(prime_set)
-    answer = len(prime_set)
+    for i in set(new_list):
+        for j in range(2,int(math.sqrt(i))+1):
+            if i % j == 0:
+                break
+        else:    
+            answer += 1
     return answer
