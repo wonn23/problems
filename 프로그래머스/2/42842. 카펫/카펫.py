09a7793ca0fcacbac659c3cce489f3ff979671f9
ChangWon Heo <wonn23@naver.com>
2023-10-14 08:38:57 +0900

    [level 2] Title: 카펫, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -1,9 +1,10 @@
+import math
 def solution(brown, yellow):
     area = brown + yellow
-    for i in range(1,area):
-        width = area//i
-        length = i
-        if width+length == (brown+4)//2 and width*length == area:
-            answer = [width,length]
-            break
-    return answer
+    for i in range(1,int(math.sqrt(yellow))+1):
+        if yellow % i == 0:
+            h = i
+            w = yellow // h
+            
+            if (w+2) * (h+2) == area:
+                return [w+2,h+2]
