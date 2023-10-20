b44706fde22df2e77ed2abdccbc2e138b0ce79c1
ChangWon Heo <wonn23@naver.com>
2023-10-20 10:21:46 +0900

    [level 2] Title: 전화번호 목록, Time: 91.64 ms, Memory: 28.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -0,0 +1,7 @@
+def solution(phone_book):
+    phone_book = sorted(phone_book)
+    
+    for i in range(1,len(phone_book)):
+        if phone_book[i].startswith(phone_book[i-1]):
+                return False      
+    return True
