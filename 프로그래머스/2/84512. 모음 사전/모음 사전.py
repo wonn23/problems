f7d04b987afbbe344399b28253a8676a109a59de
ChangWon Heo <wonn23@naver.com>
2023-10-24 00:43:35 +0900

    [level 2] Title: 모음 사전, Time: 1.73 ms, Memory: 10.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.py
@@ -0,0 +1,17 @@
+from itertools import product
+def solution(word):
+
+    vowels = ['A', 'E', 'I', 'O', 'U']
+
+    # 중복순열 계산
+    sorted_vowels = sorted(list(product(vowels, repeat=1)) +
+                           list(product(vowels, repeat=2)) +
+                           list(product(vowels, repeat=3)) +
+                           list(product(vowels, repeat=4)) +
+                           list(product(vowels, repeat=5)))
+
+    # 정리된 값들
+    cleaned_values = [''.join(item) if len(item) > 1 else item[0] for item in sorted_vowels]
+
+    
+    return cleaned_values.index(word)+1
