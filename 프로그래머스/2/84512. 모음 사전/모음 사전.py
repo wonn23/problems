07db8fd749ccd90931f0b58a64e549d23684e494
ChangWon Heo <wonn23@naver.com>
2023-10-24 10:12:26 +0900

    [level 2] Title: 모음 사전, Time: 1.39 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.py
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.py
@@ -4,17 +4,21 @@ def solution(word):
     vowels = ['A', 'E', 'I', 'O', 'U']
 
     # 중복순열 계산
-    sorted_vowels = sorted(list(product(vowels, repeat=1)) +
-                           list(product(vowels, repeat=2)) +
-                           list(product(vowels, repeat=3)) +
-                           list(product(vowels, repeat=4)) +
-                           list(product(vowels, repeat=5)))
-
+    #sorted_vowels = sorted(list(product(vowels, repeat=1)) +
+     #                      list(product(vowels, repeat=2)) +
+      #                     list(product(vowels, repeat=3)) +
+       #                    list(product(vowels, repeat=4)) +
+        #                   list(product(vowels, repeat=5)))
+        
+    sorted_vowels = []
+    for i in range(1,6):
+        sorted_vowels += list(product(vowels,repeat = i))
+        
     # 정리된 값들
     cleaned_values = []
     for i in sorted_vowels:
         cleaned_values.append(''.join(i))
-    # cleaned_values = [''.join(item) if len(item) > 1 else item[0] for item in sorted_vowels]
-
+    # cleaned_values = [''.join(item) for item in sorted_vowels]
+    cleaned_values.sort()
     
     return cleaned_values.index(word)+1
