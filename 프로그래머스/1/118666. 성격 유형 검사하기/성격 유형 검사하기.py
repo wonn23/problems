c2ad8b6faa3f94ed433141d95e27f4d1b9b7464e
ChangWon Heo <wonn22@gmail.com>
2023-11-07 10:21:55 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.18 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.py
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.py
@@ -3,6 +3,7 @@ def solution(survey, choices):
     personality = {}
     for i in 'RTCFJMAN':
         personality[i] = 0          # 성격 해시 만들기
+    print(personality)
     score = [0,3,2,1,0,1,2,3]       # 점수 리스트 만들기
     
     for i in range(len(choices)):
@@ -10,8 +11,9 @@ def solution(survey, choices):
             personality[survey[i][0]] += score[choices[i]]
         elif choices[i] >= 5:
             personality[survey[i][1]] += score[choices[i]]
-    answer += 'RT'[personality['R']<personality['T']]
-    answer += 'CF'[personality['C']<personality['F']]
-    answer += 'JM'[personality['J']<personality['M']]
-    answer += 'AN'[personality['A']<personality['N']]
+    print(personality)
+    answer += 'RT'[personality['R']<personality['T']] # 'RT'[True]
+    answer += 'CF'[personality['C']<personality['F']] # 'CF'[False]
+    answer += 'JM'[personality['J']<personality['M']] # 'CM'[True]
+    answer += 'AN'[personality['A']<personality['N']] # 'AN'[False]
     return answer
