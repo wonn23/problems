2b5564e3cea553393d4a29982e0f96e8c41bd74c
ChangWon Heo <wonn22@gmail.com>
2024-03-06 19:52:25 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 붕대 감기, Time: 0.32 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.py
+++ 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.py
@@ -1,36 +1,31 @@
 def solution(bandage, health, attacks):
-    time = attacks[-1][0]
-    continue_success = 0
-    max_health = health
-    casting_time, heal_per_second,  plus_heal = bandage
-    # badage = [시전 시간, 초당 회복량, 추가 회복량]
-    # casting_time, heal_per_second,  plus_heal = banage
-    # health = 최대 체력
-    # attcks = [공격 시간, 피해량]
-    # attack_time, damage
-    # return 몬스터의 마지막 공격직후의 체력 or -1(체력 0이하)
     # 예외 1 체력 회복 시 최대 체력을 넘어선 경우
     # 예외 2 몬스터의 마지막 공격전에 체력이 0 이하가 된 경우
+
+    continue_success = 0 # 연속 성공
+    current_health = health # 현재 체력
+    casting_time, heal_per_second,  plus_heal = bandage
+    attack_second = [attack_time[0] for attack_time in attacks]
+    attack = False
     
-    for current_time in range(time+1):
-        if health > max_health:
-            health = max_health
-        for attack_time, damage in attacks:
-            if current_time == attack_time:
-                health -= damage
-                continue_success = 0
-                break
+    for second in range(1, attack_second[-1]+1):
+        if second in attack_second:
+            attack = True
+            current_health -= attacks[attack_second.index(second)][1] # 데미지 입음
+            continue_success = 0
         else:
-            if health <= 0:
-                return -1
-            if health < max_health:
-                health += heal_per_second
-                if continue_success == casting_time:
-                    health += plus_heal
-                    continue_success = 0
+            attack = False
+            current_health += heal_per_second
+            if continue_success == casting_time:
+                current_health += plus_heal
+                continue_success = 0
+            if current_health > health:
+                current_health = health # 체력 보정
+            
                 
         continue_success += 1
+        
+        if current_health <= 0:
+                return -1
     
-    if health <= 0:
-        return -1
-    return health
+    return current_health
