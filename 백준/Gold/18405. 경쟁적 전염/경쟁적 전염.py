cde0009b4b19db8469f74c1f179335b01a365bcc
ChangWon Heo <wonn22@gmail.com>
2024-06-24 16:10:12 +0900

    [Gold V] Title: 경쟁적 전염, Time: 132 ms, Memory: 34072 KB -BaekjoonHub

--- 백준/Gold/18405. 경쟁적 전염/경쟁적 전염.py
+++ 백준/Gold/18405. 경쟁적 전염/경쟁적 전염.py
@@ -1,16 +1,10 @@
 import sys
 from collections import deque
 
-""" 
-  접근 방법은 n * n 배열을 만들고 바이러스가 퍼져나가는 로직을 작성하려고함
-  그런데 낮은 수부터 퍼져나가야하는 것을 어떻게 컨트롤 해야하는지 모르겠음.
-  이때, 배열 index에 바이러스 번호를 추가함. 1번부터 k번까지의 바이러스 번호를 돌면서 배열을 채움.
-  s 초 반복함 -> x,y 배열 데이터를 출력함.
-"""
-
 input = sys.stdin.readline
 
 n, k = map(int, input().split())
+
 lab = []
 viruses = []
 for i in range(n):
@@ -18,29 +12,28 @@ for i in range(n):
     lab.append(row)
     for j in range(n):
         if row[j] != 0:
-            viruses.append((row[j], i, j))
+            viruses.append((lab[i][j], i, j))
 
 s, x, y = map(int, input().split())
 
-
 viruses.sort()
-
-q = deque(viruses)
 
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
+
+q = deque(viruses)
 
 time = 0
 while q:
     if time == s:
         break
     for _ in range(len(q)):
-        virus_type, i, j = q.popleft()
+        virus, i, j = q.popleft()
         for idx in range(4):
             nx = i + dx[idx]
             ny = j + dy[idx]
             if 0 <= nx < n and 0 <= ny < n and lab[nx][ny] == 0:
-                lab[nx][ny] = virus_type
-                q.append((virus_type, nx, ny))
+                lab[nx][ny] = virus
+                q.append((virus, nx, ny))
     time += 1
 print(lab[x - 1][y - 1])
